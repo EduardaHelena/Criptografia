ec512f201d29ab100d0aac2a45c74eea48817489
Eduarda Helena <dudinhahelenaa@gmail.com>
2016-08-04 18:34:44 -0300

    Versão 2
    Criptografando, mas não ta imprimindo se a pessoa entrou, ou seja, tem a
    função de compar mas não está comparando.

---
+++ Criptografia/Banana.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criptografia
+{
+    class Banana
+    {
+       public  string senha;
+       
+        
+        public string getMD5Hash(string senha)
+        {
+                System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+                byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(senha);
+                byte[] hash = md5.ComputeHash(inputBytes);
+                System.Text.StringBuilder sb = new System.Text.StringBuilder();
+      
+        for (int i = 0; i < hash.Length; i++)
+        {
+             sb.Append(hash[i].ToString("X2"));
+        }
+        
+           return sb.ToString();
+        }
+
+       
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Criptografia/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Criptografia
+{
+   
+    class Program
+    {
+        static string segredo;
+        static string confirma;
+        static bool codigo;
+
+        Banana p = new Banana();
+        static void Main(string[] args)
+        {
+            
+               Console.WriteLine("------------------------- Menu ------------------------");
+               Console.WriteLine("||                                                   ||");
+               Console.WriteLine("||                                                   ||");
+               Console.WriteLine("||                                                   ||");
+               Console.WriteLine("||                                                   ||");
+               Console.WriteLine("||                                                   ||");
+               Console.WriteLine("|| Q ----> Criar senha                               ||");
+               Console.WriteLine("|| R ----> Sair                                      ||");
+              
+              ConsoleKey currentOption = Console.ReadKey().Key;
+               
+               switch (currentOption)
+               {
+                   case ConsoleKey.Q:
+                        Embaralhar();
+                        Console.ReadKey();
+                   break;
+
+                   case ConsoleKey.R:
+                        Environment.Exit(2);
+                   break;
+
+                   case ConsoleKey.P:
+                        letras();
+                        
+                   break;
+
+
+                   default:
+                       Console.WriteLine("Erro");
+                       Console.ReadKey(); 
+                       break;
+               }
+              
+      }  
+            
+            public static void Embaralhar()
+            {
+                Console.Clear();
+                Banana p = new Banana();
+
+                Console.Write("Informe o sua senha: ");
+                p.senha = Console.ReadLine();
+
+                segredo = p.getMD5Hash(p.senha);
+                Console.WriteLine(segredo);
+
+                Console.WriteLine("Confirme sua senha");
+                p.senha = Console.ReadLine();
+
+                confirma = p.getMD5Hash(p.senha);
+                Console.WriteLine(confirma);
+
+                Console.WriteLine("Aperte P, para logar");
+ 
+
+               }
+             
+            
+                public static bool letras ()
+            {
+               
+                bool codigo = (String.Compare(segredo, confirma, true) == 0);
+                if (codigo == true)
+                {
+                    Console.WriteLine("Entre");
+                    Console.ReadKey();
+                    
+                }
+                
+                return codigo; 
+                
+             
+            }  
+
+            
+      }
+     }
+
+    
+    
+            
+
+    
+
